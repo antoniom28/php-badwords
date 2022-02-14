14439ecbc2e3a25d8228d308059aa66bc19791d9
antoniom28 <antomarc@gmail.com>
2022-02-14 14:22:56 +0100

    esercitazione

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+    $text = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit officiis repellat ad doloribus accusantium libero asperiores, et quo. Itaque architecto reiciendis enim ex et. Recusandae necessitatibus soluta corporis veritatis provident.";
+    $censure = $_GET["censure"];
+
+    $text = str_replace($censure, '***' ,$text);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p> <?php echo $text; ?></p>
+    <p> <?php echo strlen($text) ?> parole </p>
+</body>
+</html>
